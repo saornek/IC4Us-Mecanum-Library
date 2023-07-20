23e50fbf5f288475ad76a387e835055202c90ffa
Selin Alara Ornek <47495313+saornek@users.noreply.github.com>
2023-07-20 12:18:19 +0300

    +created control_test.py

---
+++ control_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""
+Author: saornek
+Date: *
+Last Updated: 07/20/2023
+Status: working
+Purpose: Created for IC4U3 - Mecanum motor control with Adafruit TB6612
+Notes: Mecanum Movements Inspired by https://www.researchgate.net/profile/Jin-Gang-Jiang-2/publication/307868549/figure/fig2/AS:713745810599936@1547181670156/Fig-5-Top-view-of-turning-principle-of-Mecanum-wheel.png
+"""
